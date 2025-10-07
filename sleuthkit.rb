4ebaba083aac6055fee93079df59be9285df36a4
Mark McKinnon <Mark.McKinnon13@outlook.com>
2025-10-07 14:31:45 -0400

    Update sleuthkit.rb
    Update url

--- sleuthkit.rb
+++ sleuthkit.rb
@@ -1,7 +1,7 @@
 class Sleuthkit < Formula
   desc "Installs Sleuthkit supporting libraries and Java jars for Autopsy"
   homepage "https://sleuthkit.org"
-  url "https://github.com/markmckinnon/homebrew-sleuthkit/blob/main/sleuthkit-macos-aarch64.tar.gz"
+  url "https://github.com/markmckinnon/homebrew-sleuthkit/raw/refs/heads/main/sleuthkit-macos-aarch64.tar.gz"
   sha256 "d9a6c45b4c0582bbd1f6cf58f650a61057a481ac603e2158be54973aa4f8f0c8"
   version "4.14.0"
 
